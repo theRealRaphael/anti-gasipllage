24dfe594c64449b2a6b0897cb567099f3c98d09e
theRealRaphael <deze.raphael@gmail.com>
2022-05-07 17:54:57 +0200

    ajout des fichiers de base

--- README.md
+++
@@ -1,2 +0,0 @@
-# anti-gasipllage
-Une application web utilisant Spring, ainsi que des Servlets, JSP et JDBC réalisé dans le cadre des études
---
+++ src/main/java/com/example/antigaspillage/AntiGaspillageApplication.java
@@ -0,0 +1,13 @@
+package com.example.antigaspillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiGaspillageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntiGaspillageApplication.class, args);
+    }
+
+}
